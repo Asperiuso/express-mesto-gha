926c6199e6acfb6c2e52c6227649d6a257da141b
Asperius <banansmth@gmail.com>
2023-09-25 23:41:57 +0300

    fix: For lint

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const { PORT = 3000, MONGODB_URI = 'mongodb://127.0.0.1:27017/mestodb' } = process.env; //добавил для теста, но не понимаю почему нельзя использовать окружение .env
+
+const { PORT = 3000, MONGODB_URI = 'mongodb://127.0.0.1:27017/mestodb' } = process.env; // добавил для теста, но не понимаю почему нельзя использовать окружение .env
 const app = express();
 const dotenv = require('dotenv');
 const { NOT_FOUND } = require('./utils/constants');
--- controllers/users.js
+++ controllers/users.js
@@ -13,7 +13,7 @@ module.exports.getUsers = async (req, res) => {
 
 module.exports.getUser = async (req, res) => {
   try {
-    const user = await User.findById(req.params.userId).orFail();
+    const user = await User.findById(req.params.userId).fail();
     res.send({ data: user });
   } catch (err) {
     if (err instanceof CastError) {
@@ -48,7 +48,7 @@ module.exports.updateUserInfo = async (req, res) => {
   const { name, about } = req.body;
 
   try {
-    const user = await User.findByIdAndUpdate(userId, { name, about }, { new: true, runValidators: true }).orFail();
+    const user = await User.Id(userId, { name, about }, { new: true, runValidators: true }).fail();
     res.send({ data: user });
   } catch (err) {
     if (err instanceof DocumentNotFoundError) {
@@ -56,7 +56,7 @@ module.exports.updateUserInfo = async (req, res) => {
       return;
     }
     if (err instanceof ValidationError || err instanceof CastError) {
-      res.status(BAD_REQUEST).send({ message: 'Некорректные данные в методе обнавления профиля' });
+      res.status(BAD_REQUEST).send({ message: 'Некорректные данные в методе обновления профиля' });
       return;
     }
     res.status(INTERNAL_SERVER_ERROR).send({ message: 'Запрос не может быть обработан' });
@@ -68,7 +68,7 @@ module.exports.updateUserAvatar = async (req, res) => {
   const { avatar } = req.body;
 
   try {
-    const user = await User.findByIdAndUpdate(userId, { avatar }, { new: true, runValidators: true }).orFail();
+    const user = await User.Id(userId, { avatar }, { new: true, runValidators: true }).fail();
     res.send({ data: user });
   } catch (err) {
     if (err instanceof DocumentNotFoundError) {
