7cc6dcb8dbbef05f9a36b7e57d6de4640a276784
Asperius <banansmth@gmail.com>
2023-10-01 22:20:14 +0300

    fix: Доработать роутс

--- routes/users.js
+++ routes/users.js
@@ -1,35 +1,17 @@
 const router = require('express').Router();
-const { celebrate, Joi } = require('celebrate');
-const { URL_PATTERN } = require('../utils/constants');
 
 const {
   getUsers,
   getUser,
-  getCurrentUser,
   updateUserInfo,
   updateUserAvatar,
 } = require('../controllers/users');
+const { validationProfile, validationId, validationAvatar } = require('../utils/validation');
 
-router.get('/', getUsers);
-router.get('/me', getCurrentUser);
-
-router.get('/:userId', celebrate({
-  params: Joi.object().keys({
-    userId: Joi.string().length(24).hex().required(),
-  }),
-}), getUser);
-
-router.patch('/me', celebrate({
-  body: Joi.object().keys({
-    name: Joi.string().min(2).max(30),
-    about: Joi.string().min(2).max(30),
-  }),
-}), updateUserInfo);
-
-router.patch('/me/avatar', celebrate({
-  body: Joi.object().keys({
-    avatar: Joi.string().pattern(URL_PATTERN),
-  }),
-}), updateUserAvatar);
+router.get('/users', getUsers);
+router.get('/users/me', getUser);
+router.get('/:userId', validationId, getUser);
+router.patch('/me', validationProfile, updateUserInfo);
+router.patch('/me/avatar', validationAvatar, updateUserAvatar);
 
 module.exports = router;
