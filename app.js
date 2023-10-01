329ca08ea60f62aafb522c97487e3a13ebb6cb06
Asperius <banansmth@gmail.com>
2023-10-01 19:13:06 +0300

    fix: У ясеня

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
 const { celebrate, Joi } = require('celebrate');
 
 const { PORT = 3000, MONGODB_URI = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 const app = express();
 const dotenv = require('dotenv');
 const usersRouter = require('./routes/users');
-const { NOT_FOUND, URL_PATTERN } = require('./utils/constants');
+const { NOT_FOUND, URL_PATTERN, INTERNAL_SERVER_ERROR } = require('./utils/constants');
 const auth = require('./middlewares/auth');
 const errorHandler = require('./middlewares/error-handler');
 const { signin, signup } = require('./controllers/users');
@@ -16,12 +17,10 @@ mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
 });
 
-app.post('/signin', celebrate({
-  body: Joi.object().keys({
-    email: Joi.string().required().email(),
-    password: Joi.string().required().min(6),
-  }),
-}), signin);
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cookieParser());
+
 app.post('/signup', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
@@ -32,14 +31,24 @@ app.post('/signup', celebrate({
   }),
 }), signup);
 
+// Маршрут для входа пользователя
+app.post('/signin', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(6),
+  }),
+}), signin);
+
 app.use(auth);
 
-app.use(express.json());
 app.use('/users', usersRouter);
-app.use(express.urlencoded({ extended: true }));
-
-app.use('/users', require('./routes/users'));
 app.use('/cards', require('./routes/cards'));
+
+app.use((err, req, res) => {
+  // Обработка ошибок
+  console.error(err);
+  res.status(INTERNAL_SERVER_ERROR).json({ error: 'Internal Server Error' });
+});
 
 app.use((req, res) => {
   res.status(NOT_FOUND).send({ message: 'Путь не найден' });
