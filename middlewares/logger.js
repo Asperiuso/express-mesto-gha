531463b5f552ff7f3ba22ba89acf36caeb32b708
Asperius <banansmth@gmail.com>
2023-10-07 12:57:53 +0300

    fix: го

--- middlewares/logger.js
+++ middlewares/logger.js
@@ -1,37 +1,16 @@
 const winston = require('winston');
 const expressWinston = require('express-winston');
 
-// Уровень журналирования (по умолчанию, 'info')
-const logLevel = process.env.NODE_ENV === 'production' ? 'info' : 'debug';
-
-// Создание и настройка логгера для запросов
 const requestLogger = expressWinston.logger({
   transports: [
-    new winston.transports.File({ filename: 'request.log', level: 'info' }),
-    new winston.transports.Console({
-      format: winston.format.combine(
-        winston.format.colorize(),
-        winston.format.simple(),
-      ),
-      level: logLevel,
-    }),
+    new winston.transports.File({ filename: 'request.log' }),
   ],
   format: winston.format.json(),
-  meta: true, // Включение мета-данных (например, URL запроса)
-  expressFormat: true, // Форматирование сообщений, чтобы они соответствовали журналам Express
 });
 
-// Создание и настройка логгера для ошибок
 const errorLogger = expressWinston.errorLogger({
   transports: [
-    new winston.transports.File({ filename: 'error.log', level: 'error' }),
-    new winston.transports.Console({
-      format: winston.format.combine(
-        winston.format.colorize(),
-        winston.format.simple(),
-      ),
-      level: logLevel,
-    }),
+    new winston.transports.File({ filename: 'error.log' }),
   ],
   format: winston.format.json(),
 });
